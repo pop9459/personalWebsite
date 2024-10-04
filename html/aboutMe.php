e7008b3182c34105a87d6e5908690ff722ae3bc7
Peter Kapsiar <kapsiar888n@gmail.com>
2024-10-04 14:47:21 +0200

    added test text to about me

--- html/aboutMe.php
+++ html/aboutMe.php
@@ -1,5 +1,7 @@
 <div class="widget">
     <h1>About Me</h1>
     <h3>Describe yourself with 4 words</h3>
-    <p>Genius, billionaire, playboy, philanthropist.</p>    
+    <p>Genius, billionaire, playboy, philanthropist.</p> 
+    
+    <h3>nothing is impossible, but at some point it becomes too expensive</h3>
 </div>
