c6c537e38787efa17c34b802dbbabe1a56f6a1d2
Peter Kapsiar <kapsiar888n@gmail.com>
2024-11-07 08:40:45 +0100

    animated about me widget

--- html/aboutMe.php
+++ html/aboutMe.php
@@ -1,9 +1,7 @@
-<div class="widget">
+<div class="widget expandVertical">
     <h1>About Me</h1>
     <h3>Describe yourself with 4 words</h3>
     <p>Genius, billionaire, playboy, philanthropist.</p> 
     
-    <p>nothing is impossible, but at some point it becomes too expensive</p>
-
-    <p>test text</p>
+    <p>Nothing is impossible, but at some point it becomes too expensive</p>
 </div>      
