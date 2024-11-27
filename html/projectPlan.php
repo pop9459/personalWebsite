ab95f19da2b97798379cd63f29bd1221e1c376ec
Peter Kapsiar <kapsiar888n@gmail.com>
2024-11-27 15:41:54 +0100

    added forgoten files

---
+++ html/projectPlan.php
@@ -0,0 +1,14 @@
+<div class="widget">
+    <h1>Project Plan</h1>
+    <div class="inLine">
+        <h4>Competence:</h4>
+        <p>Ethics and project based work practices</p>
+    </div>
+</div>
+<div class="widget">
+    <p>This is a project plan that me and my team created for our period one project</p>
+    <p>The projects main focus was to create a functional website for a fictional client</p>
+</div>
+<div class="widget">
+    <embed src="profSkillsPortfolio/projectPlan/SunnySocksProjectPlan.pdf" type="application/pdf">
+</div>
