458c3ef8678fe021e4f5cf5948893601b526d8ca
Peter Kapsiar <kapsiar888n@gmail.com>
2024-11-27 15:03:51 +0100

    added project plan to portfolio

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -7,8 +7,43 @@ export function clearContents(containerID)
 }
 
 export function setupListeners() {
+    // MENU BUTTONS
     document.getElementById("aboutMeButton").onclick = function() { 
         loadToContainer("centerContentDisplay" ,"html/aboutMe.php")
+        .then(() => animateContainerWidgets("centerContentDisplay"));
+    };
+
+    // PORTFOLIO BUTTONS
+    document.getElementById("reflectionReportButton").onclick = function() {
+        loadToContainer("centerContentDisplay", "html/aboutMe.php")
+        .then(() => animateContainerWidgets("centerContentDisplay"));
+    };
+    document.getElementById("studyCareerCoachingButton").onclick = function() {
+        loadToContainer("centerContentDisplay", "html/aboutMe.php")
+        .then(() => animateContainerWidgets("centerContentDisplay"));
+    };
+    document.getElementById("feedbackFormsButton").onclick = function() {
+        loadToContainer("centerContentDisplay", "html/aboutMe.php")
+        .then(() => animateContainerWidgets("centerContentDisplay"));
+    };
+    document.getElementById("projectPlanButton").onclick = function() {
+        loadToContainer("centerContentDisplay", "html/projectPlan.php")
+        .then(() => animateContainerWidgets("centerContentDisplay"));
+    };
+    document.getElementById("communicationButton").onclick = function() {
+        loadToContainer("centerContentDisplay", "html/aboutMe.php")
+        .then(() => animateContainerWidgets("centerContentDisplay"));
+    };
+    document.getElementById("presentationButton").onclick = function() {
+        loadToContainer("centerContentDisplay", "html/aboutMe.php")
+        .then(() => animateContainerWidgets("centerContentDisplay"));
+    };
+    document.getElementById("meetingTechniquesButton").onclick = function() {
+        loadToContainer("centerContentDisplay", "html/aboutMe.php")
+        .then(() => animateContainerWidgets("centerContentDisplay"));
+    };
+    document.getElementById("edumundoAssignmentsButton").onclick = function() {
+        loadToContainer("centerContentDisplay", "html/aboutMe.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
 
