7330facf5681d914f02b52c368876a19bd5d3fd3
Peter Kapsiar <kapsiar888n@gmail.com>
2025-01-05 22:15:33 +0100

    updated scc tab

---
+++ html/studyCareerCoaching.php
@@ -0,0 +1,12 @@
+<div class="widget">
+    <h1>Study Career Coaching</h1>
+    <div class="inLine">
+        <h4>Competences:</h4>
+        <ul>
+            <li>Personal Leadership</li>
+        </ul>
+    </div>
+</div>
+<div class="widget">
+    <embed src="profSkillsPortfolio/studyCareerCoaching/Advice.pdf" type="application/pdf">
+</div>
