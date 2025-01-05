dcddcd47261e78fe1370cb89d803c526ad9864c8
Peter Kapsiar <kapsiar888n@gmail.com>
2025-01-05 21:35:22 +0100

    updated edumundo tab

---
+++ html/edumundoAssignments.php
@@ -0,0 +1,13 @@
+<div class="widget">
+    <h1>Edumundo Assignments</h1>
+    <div class="inLine">
+        <h4>Competences:</h4>
+        <ul>
+            <li>Personal leadership</li>
+            <li>Ethics and project based work practices</li>
+        </ul>
+    </div>
+</div>
+<div class="widget">
+    <embed src="profSkillsPortfolio/edumundoAssignments/Portfolio_Edumundo_2024_2025.pdf" type="application/pdf">
+</div>
