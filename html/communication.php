caa6298e7b9e3240f51eacd336f4d42456c9239d
Peter Kapsiar <kapsiar888n@gmail.com>
2025-01-05 22:03:19 +0100

    mend

---
+++ html/communication.php
@@ -0,0 +1,12 @@
+<div class="widget">
+    <h1>Communication</h1>
+    <div class="inLine">
+        <h4>Competences:</h4>
+        <ul>
+            <li>Communication</li>
+        </ul>
+    </div>
+</div>
+<div class="widget">
+    <embed src="profSkillsPortfolio/communication/interviewSchedule.pdf" type="application/pdf">
+</div>
