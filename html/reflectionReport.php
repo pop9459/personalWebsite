79508b504adad2b71dd8e9cf3c2ed0a52e95660e
Peter Kapsiar <kapsiar888n@gmail.com>
2025-01-08 02:08:49 +0100

    added a reflection report tab

---
+++ html/reflectionReport.php
@@ -0,0 +1,12 @@
+<div class="widget">
+    <h1>Reflection report</h1>
+    <div class="inLine">
+        <h4>Competences:</h4>
+        <ul>
+            <li>Personal leadership</li>
+        </ul>
+    </div>
+</div>
+<div class="widget">
+    <embed src="profSkillsPortfolio/reflectionReport/ReflectionSemester1.pdf" type="application/pdf">
+</div>
