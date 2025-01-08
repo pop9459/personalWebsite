3fdc526b2e0ff2d43c404f325affef6cbcd59dd1
Peter Kapsiar <kapsiar888n@gmail.com>
2025-01-08 02:14:05 +0100

    made the temporary about me a bit less temporary

--- html/aboutMe.php
+++ html/aboutMe.php
@@ -1,14 +1,7 @@
 <div class="widget">
     <h1>About Me</h1>
+    <p>Hello, this is Peter, as you know. I will add more stuff in this tab in the future, if I feel like it. For now here are some cringe quotes</p>
+    <p>Nothing is impossible, but at some point it becomes too expensive.</p>
     <h3>Describe yourself with 4 words</h3>
     <p>Genius, billionaire, playboy, philanthropist.</p> 
-    
-    <p>Nothing is impossible, but at some point it becomes too expensive</p>
-</div>
-<div class="widget">
-    <h1>About Me</h1>
-    <h3>Describe yourself with 4 words</h3>
-    <p>Genius, billionaire, playboy, philanthropist.</p> 
-    
-    <p>Nothing is impossible, but at some point it becomes too expensive</p>
 </div>
