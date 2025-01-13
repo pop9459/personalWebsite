4c5070730f9f32c489f297963a797908c96e6ae5
Peter Kapsiar <kapsiar888n@gmail.com>
2025-01-13 14:45:36 +0100

    added feedbacks

---
+++ phpContent/feedbackForms.php
@@ -0,0 +1,42 @@
+<div class="widget">
+    <h1>Feedbackforms</h1>
+    <div class="inLine">
+        <h4>Competences:</h4>
+        <ul>
+            <li>Personal Leadership</li>
+        </ul>
+    </div>
+</div>
+<div class="widget">
+    <h2>Bence</h2>
+    <p>I have noticed that you are quite proactive in executing tasks and taking the initiative to address them. This makes me feel reassure that you are always looking to improve and contribute to  our project. Your approach greatly enhances the team's efficiency and effectiveness. I want you to keep up this behaviour, your contributions are invaluable and greatly appreciated.</p>
+</div>
+<div class="widget">
+    <h2>Sanduni</h2>
+    <p>I noticed in the meetings with the client you asked questions and participated those meetings very actively so it made our project and other members to understand this project well and get a right idea about this project. So I hope you to continue this asking questions and participation actively in the meetings as a habit because it benefits a lot to the team.</p>
+</div>
+<div class="widget">
+    <h2>Eliza</h2>
+    <p>I observed that you went with your work with a hight level of seriousness and dedication. This set a great example for the team and motivated others, which improved the overall quality of the project. In the future, you could consider sharing some of your methods with the team, so they could learn from you. This way, you will not only inspire others but also enhance the team’s overall efficiency.</p>    
+</div>
+<div class="widget">
+    <h2>Arad</h2>
+    <p>Peter, you were an amazing guide for us during our project, always helping us with coding and teaching us about coding concepts that were new to us. Your work ethic is inspiring, and you were the glue keeping our group together throughout the whole period. I hope that in the future you continue to guide and inspire your group members, even if they don't show willingness to learn.</p>    
+</div>
+<div class="widget">
+    <h2>David</h2>
+    <p>I want to take a moment to say how much I appreciate everything you’ve done. Working with you has been such a great experience because you’re so resourceful and knowledgeable. Even though you joke about being lazy, it’s clear you put in a lot of effort and always work hard. Thanks for being so supportive and dedicated. I really enjoyed working with you, and I’m looking forward to teaming up again in the future!</p>    
+</div>
+<div class="widget">
+    <h2>Alex</h2>
+    <p>to be received</p>
+</div>
+<div class="widget">
+    <h2>Kyan</h2>
+    <p>Hi Peter, even though I didn’t work directly with you on the project since I was focused on the Windows Server and you were handling the Ubuntu Server, I could see that you did a great job. I’m really glad about how well you managed your part of the project—it made things run smoothly for the team.</p>
+    <p>I also noticed that you brought a positive and upbeat vibe to the group, which really helped create a good atmosphere. In the future, keep up the great work and continue being a source of positivity. It makes a big difference to the team’s dynamic!</p>
+</div>
+<div class="widget">
+    <h2>Ugo</h2>
+    <p>You’re a good team member, very communicative and you also try to deliver any task that was given to you on time. This also inspire me to work hard for the team and also be a better student.</p>
+</div>
