39db123837cd9c1a2b7f499c474210d7c48e22fb
Peter Kapsiar <kapsiar888n@gmail.com>
2025-01-14 09:24:46 +0100

    fixed project plan

--- phpContent/projectPlan.php
+++ phpContent/projectPlan.php
@@ -9,9 +9,5 @@
     </div>
 </div>
 <div class="widget">
-    <p>This is a project plan that me and my team created for our period one project</p>
-    <p>The projects main focus was to create a functional website for a fictional client</p>
-</div>
-<div class="widget">
-    <embed src="profSkillsPortfolio/projectPlan/SunnySocksProjectPlan.pdf" type="application/pdf">
+    <embed src="profSkillsPortfolio/projectPlan/ProjectPlanGemorskos.pdf" type="application/pdf">
 </div>
