fa3de86e2b051a28fd0dca7ed806a8b802a058b8
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 17:07:07 +0200

    added description to the reflection tab

--- phpContent/portfolio2/portfolio2Reflection.php
+++ phpContent/portfolio2/portfolio2Reflection.php
@@ -1,6 +1,12 @@
 <div class="widget">
     <h1>Portfolio 2 Reflection</h1>
-    <p>Placeholder for a short description of the reflection content.</p>
+    <p>
+        This is a reflection report on my first year of studying at IT at the university of NHL Stenden in Emmen.
+        It is mostly tailored to include all te requirements from the checklist provided by my lecturer. 
+        It touches on topics like consequences of my actions, teamwork experiences or my personal study experiences.
+        It also contains an evaluation of the results of my PDP plan. 
+        One of the PDP goals is reflected on using the STARRT method.
+    </p>
 </div>
 <div class="widget">
     <h2>Reflection Document</h2>
