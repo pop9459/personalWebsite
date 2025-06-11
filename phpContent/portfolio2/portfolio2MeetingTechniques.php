5a2e1e203bc2a36d9815154710ff44c377402b3b
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 16:03:53 +0200

    added file showcase to meeting techniques

--- phpContent/portfolio2/portfolio2MeetingTechniques.php
+++ phpContent/portfolio2/portfolio2MeetingTechniques.php
@@ -1,5 +1,23 @@
 <div class="widget">
     <h1>Portfolio 2 Meeting Techniques</h1>
     <p>Placeholder for a short description of the meeting techniques content.</p>
-    <embed src="../profSkillsPortfolio2/MeetingTechniques/index.html" width="100%" height="600px"></embed>
+</div>
+<div class="widget">
+    <h2>Meetign agenda drawn up by me</h2>
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/meetingAgenda.pdf" type="application/pdf">
+    <h3>Minutes of meeting from this agenda</h3>
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/MinutesOfMeeting-IWasChair.pdf" type="application/pdf">
+</div>
+<div class="widget">
+    <h2>Minutes of meeting written by me</h2>
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/MinutesOfMeeting-IWasSec.pdf" type="application/pdf">
+</div>
+<div class="widget">
+    <h2>Evaluation forms from my meeting</h2>
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFormNick.pdf" type="application/pdf">
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromAlex.pdf" type="application/pdf">
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromAmir.pdf" type="application/pdf">
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromJoey.pdf" type="application/pdf">
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromOliver.pdf" type="application/pdf">
+    <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromPharell.pdf" type="application/pdf">    
 </div>
