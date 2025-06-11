4b7729526cd427d7a59decd2d2b51d102be8aa46
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 17:22:59 +0200

    grammar fix

--- phpContent/portfolio2/portfolio2Feedback.php
+++ phpContent/portfolio2/portfolio2Feedback.php
@@ -1,7 +1,7 @@
 <div class="widget">
     <h1>Portfolio 2 Feedback</h1>
     <p>
-        This is a collection of six 360-degree feedback forms I have collecteed from various colleagues and peers.
+        This is a collection of six 360-degree feedback forms I have collected from various colleagues and peers.
         The purpose of this is to help me understand my myself from an external perspective.
         It may help me where other people see my strengths and weaknesses, and how I can improve in the future.
     </p>
--- phpContent/portfolio2/portfolio2MeetingTechniques.php
+++ phpContent/portfolio2/portfolio2MeetingTechniques.php
@@ -3,7 +3,7 @@
     <p>Placeholder for a short description of the meeting techniques content.</p>
 </div>
 <div class="widget">
-    <h2>Meetign agenda drawn up by me</h2>
+    <h2>Meeting agenda drawn up by me</h2>
     <embed src="src/profSkillsPortfolio2/MeetingTechniques/meetingAgenda.pdf" type="application/pdf">
     <h3>Minutes of meeting from this agenda</h3>
     <embed src="src/profSkillsPortfolio2/MeetingTechniques/MinutesOfMeeting-IWasChair.pdf" type="application/pdf">
