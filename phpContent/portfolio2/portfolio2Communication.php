68264695dacf089fc1b9b367d5038acf4bc939ea
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 18:58:11 +0200

    added descripton for communication tab

--- phpContent/portfolio2/portfolio2Communication.php
+++ phpContent/portfolio2/portfolio2Communication.php
@@ -1,6 +1,10 @@
 <div class="widget">
     <h1>Portfolio 2 Communication</h1>
-    <p>Placeholder for a short description of the communication content.</p>
+    <p>
+        This is a collection of communication evaluation forms.
+        Communication is a key to a successful collaborative environment. These forms were filled out by my project innovate team members. 
+        By looking at these forms I can determine if I am communicating effectively with my peers and colleagues and where I can improve.
+    </p>
 </div>
 <div class="widget">
     <embed src="src/profSkillsPortfolio2/Communication/CommunicationEvaluationForm.pdf" type="application/pdf">
