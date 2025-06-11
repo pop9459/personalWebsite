76af5fa126a39e5822bd7f4b7244503760228c75
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 19:13:58 +0200

    added edumundo tab description

--- phpContent/portfolio2/portfolio2EdumundoAssignments.php
+++ phpContent/portfolio2/portfolio2EdumundoAssignments.php
@@ -1,6 +1,10 @@
 <div class="widget">
     <h1>Portfolio 2 Edumundo Assignments</h1>
-    <p>Placeholder for a short description of the Edumundo assignments content.</p>
+    <p>
+        This is an export of the Edumundo assignments that are requested for the second professional skills portfolio.
+        Doing these assignments I gain valuable knowledge required for my professional development.
+        The assignments cover various topics such as goal-setting, time management, and feedback.
+    </p>
 </div>
 <div class="widget">
     <embed src="src/profSkillsPortfolio2/EdumundoAssignments/Edumundo.pdf" type="application/pdf">
