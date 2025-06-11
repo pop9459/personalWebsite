18cb68d10f96783d7a8b158cc9d6e86b28fdb9b3
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 16:46:18 +0200

    added file showcase to the reflection tab

--- phpContent/portfolio2/portfolio2Reflection.php
+++ phpContent/portfolio2/portfolio2Reflection.php
@@ -1,5 +1,8 @@
 <div class="widget">
     <h1>Portfolio 2 Reflection</h1>
     <p>Placeholder for a short description of the reflection content.</p>
-    <embed src="../profSkillsPortfolio2/Reflection/index.html" width="100%" height="600px"></embed>
+</div>
+<div class="widget">
+    <h2>Reflection Document</h2>
+    <embed src="src/profSkillsPortfolio2/Reflection/ReflectionReportYearOne.pdf" type="application/pdf">
 </div>
