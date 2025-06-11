aea6c213ecc7ba986e4568d2a5eed30febed87f7
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 16:45:22 +0200

    added file showcase to project plan tab

--- phpContent/portfolio2/portfolio2ProjectPlan.php
+++ phpContent/portfolio2/portfolio2ProjectPlan.php
@@ -1,5 +1,8 @@
 <div class="widget">
     <h1>Portfolio 2 Project Plan</h1>
     <p>Placeholder for a short description of the project plan content.</p>
-    <embed src="../profSkillsPortfolio2/ProjectPlan/index.html" width="100%" height="600px"></embed>
+</div>
+<div class="widget">
+    <h2>Project Plan Document</h2>
+    <embed src="src/profSkillsPortfolio2/ProjectPlan/ProjectPlanIT1A.pdf" type="application/pdf">
 </div>
