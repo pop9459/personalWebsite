81683fe7e3aaa9308f97c863dd418c707473cf99
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 16:57:48 +0200

    Added file displays to feedback tab

--- phpContent/portfolio2/portfolio2Feedback.php
+++ phpContent/portfolio2/portfolio2Feedback.php
@@ -1,5 +1,24 @@
 <div class="widget">
     <h1>Portfolio 2 Feedback</h1>
     <p>Placeholder for a short description of the feedback content.</p>
-    <embed src="../profSkillsPortfolio2/Feedback/index.html" width="100%" height="600px"></embed>
+</div>
+<div class="widget">
+    <h2>Feedback from Alex</h2>
+    <embed src="src/profSkillsPortfolio2/Feedback/360fromAlex.pdf" type="application/pdf">
+</div>
+<div class="widget">
+    <h2>Feedback from David</h2>
+    <embed src="src/profSkillsPortfolio2/Feedback/360fromDavid.pdf" type="application/pdf">
+</div>
+<div class="widget">
+    <h2>Feedback from Eliza</h2>
+    <embed src="src/profSkillsPortfolio2/Feedback/360fromEliza.pdf" type="application/pdf">
+</div>
+<div class="widget">
+    <h2>Feedback from Oliver</h2>
+    <embed src="src/profSkillsPortfolio2/Feedback/360fromOliver.pdf" type="application/pdf">
+</div>
+<div class="widget">
+    <h2>Feedback from Pharell</h2>
+    <embed src="src/profSkillsPortfolio2/Feedback/360fromPharell.pdf" type="application/pdf">
 </div>
