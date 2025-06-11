2b81b673368896454331fb04d9d666ba054b5258
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 15:26:00 +0200

    organised php content files into separate folders

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -15,73 +15,73 @@ export function setupListeners() {
 
     // PORTFOLIO 2 BUTTONS
     document.getElementById("portfolio2ReflectionButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2Reflection.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2Reflection.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("portfolio2FeedbackButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2Feedback.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2Feedback.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("portfolio2PDPButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2PDP.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2PDP.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("portfolio2ProjectPlanButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2ProjectPlan.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2ProjectPlan.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("portfolio2CommunicationButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2Communication.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2Communication.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("portfolio2PresentingButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2Presenting.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2Presenting.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("portfolio2EdumundoAssignmentsButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2EdumundoAssignments.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2EdumundoAssignments.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("portfolio2MeetingTechniquesButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2MeetingTechniques.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2MeetingTechniques.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("portfolio2FinalInterviewButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/portfolio2FinalInterview.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio2/portfolio2FinalInterview.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
 
     // PORTFOLIO 1 BUTTONS
     document.getElementById("reflectionReportButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/reflectionReport.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio1/reflectionReport.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("studyCareerCoachingButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/studyCareerCoaching.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio1/studyCareerCoaching.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("feedbackFormsButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/feedbackForms.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio1/feedbackForms.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("projectPlanButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/projectPlan.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio1/projectPlan.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("communicationButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/communication.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio1/communication.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("presentationButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/presentation.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio1/presentation.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("meetingTechniquesButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/meetingTechniques.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio1/meetingTechniques.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
     document.getElementById("edumundoAssignmentsButton").onclick = function() {
-        loadToContainer("centerContentDisplay", "phpContent/edumundoAssignments.php")
+        loadToContainer("centerContentDisplay", "phpContent/portfolio1/edumundoAssignments.php")
         .then(() => animateContainerWidgets("centerContentDisplay"));
     };
 
