0831002cd1e6414b79f51abf689bfc2c3a1952a7
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 20:25:15 +0200

    removed description for final interview

--- phpContent/portfolio2/portfolio2FinalInterview.php
+++ phpContent/portfolio2/portfolio2FinalInterview.php
@@ -1,6 +1,5 @@
 <div class="widget">
     <h1>Portfolio 2 Final Interview</h1>
-    <p>Placeholder for a short description of the final interview content.</p>
 </div>
 <div class="widget">
     <embed src="src/profSkillsPortfolio2/FinalInterview/FinalInterview.pdf" type="application/pdf">
