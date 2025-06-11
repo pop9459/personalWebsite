32da10163a2bedb53502f900520e97709444b2a5
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 16:41:11 +0200

    added file showcase to PDP tab

--- phpContent/portfolio2/portfolio2PDP.php
+++ phpContent/portfolio2/portfolio2PDP.php
@@ -1,5 +1,27 @@
 <div class="widget">
     <h1>Portfolio 2 PDP</h1>
     <p>Placeholder for a short description of the PDP content.</p>
-    <embed src="../profSkillsPortfolio2/PDP/index.html" width="100%" height="600px"></embed>
+</div>
+<div class="widget">
+    <h2>My PDP</h2>
+    <embed src="src/profSkillsPortfolio2/PDP/PDP_v3.pdf" type="application/pdf">
+</div>
+<div class="widget">
+    <h2>PDP results</h2>
+</div>
+<div class="widget">
+    <h3>Development goal 1:</h3>
+    <p>Goal result description</p>
+    <a href="https://github.com/Pharrellstu/Project-Innovate/tree/develop/Campus-Link/campuslink">Flutter app I helped to levelop</a>
+</div>
+<div class="widget">
+    <h3>Development goal 2:</h3>
+    <p>Goal result description</p>
+    <img src="src/profSkillsPortfolio2/PDP/duolingoMay.jpg" alt="duolingoCalendarMay">
+    <img src="src/profSkillsPortfolio2/PDP/duolingoJune.jpg" alt="duolingoCalendarJune">
+</div>
+<div class="widget">    
+    <h3>Development goal 3:</h3>
+    <p>Goal result description</p>
+    <embed src="src/profSkillsPortfolio2/PDP/ExerciseTracker.pdf" type="application/pdf">
 </div>
