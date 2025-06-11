4b8c6d432154bb6a71bda181817b1ef6611860a9
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 15:40:31 +0200

    added file display for Communication

--- phpContent/portfolio2/portfolio2Communication.php
+++ phpContent/portfolio2/portfolio2Communication.php
@@ -1,5 +1,7 @@
 <div class="widget">
     <h1>Portfolio 2 Communication</h1>
     <p>Placeholder for a short description of the communication content.</p>
-    <embed src="../profSkillsPortfolio2/Communication/index.html" width="100%" height="600px"></embed>
+</div>
+<div class="widget">
+    <embed src="src/profSkillsPortfolio2/Communication/CommunicationEvaluationForm.pdf" type="application/pdf">
 </div>
