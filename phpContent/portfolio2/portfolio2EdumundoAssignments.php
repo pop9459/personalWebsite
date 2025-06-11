b8c38c21c329cc835e024657a9eb1bf422b43fa3
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 15:43:06 +0200

    added edumundo file display

--- phpContent/portfolio2/portfolio2EdumundoAssignments.php
+++ phpContent/portfolio2/portfolio2EdumundoAssignments.php
@@ -1,5 +1,7 @@
 <div class="widget">
     <h1>Portfolio 2 Edumundo Assignments</h1>
     <p>Placeholder for a short description of the Edumundo assignments content.</p>
-    <embed src="../profSkillsPortfolio2/EdumundoAssignments/index.html" width="100%" height="600px"></embed>
+</div>
+<div class="widget">
+    <embed src="src/profSkillsPortfolio2/EdumundoAssignments/Edumundo.pdf" type="application/pdf">
 </div>
