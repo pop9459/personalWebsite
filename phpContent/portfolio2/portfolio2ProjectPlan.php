76f6b6fdfabcc496af9784a5ef209c545ed48469
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 19:03:27 +0200

    added temp project plan description

--- phpContent/portfolio2/portfolio2ProjectPlan.php
+++ phpContent/portfolio2/portfolio2ProjectPlan.php
@@ -1,6 +1,9 @@
 <div class="widget">
     <h1>Portfolio 2 Project Plan</h1>
-    <p>Placeholder for a short description of the project plan content.</p>
+    <p>
+        This is the project plan I created for my project Innovate during the fourth period.
+        TODO - add advice report
+    </p>
 </div>
 <div class="widget">
     <h2>Project Plan Document</h2>
