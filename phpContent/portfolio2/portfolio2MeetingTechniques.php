702ca8733240e05b993a54ced0b066fd3170a8d1
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-11 20:43:40 +0200

    added description for meeting techniques tab + organising widgets

--- phpContent/portfolio2/portfolio2MeetingTechniques.php
+++ phpContent/portfolio2/portfolio2MeetingTechniques.php
@@ -1,6 +1,11 @@
 <div class="widget">
     <h1>Portfolio 2 Meeting Techniques</h1>
-    <p>Placeholder for a short description of the meeting techniques content.</p>
+    <p>
+        This is a collection of documents related to meetings I was a part of during the last period.
+        To convey information and make decisions collectively, meetings are a crucial part of any project.
+        Here I have listed the meeting agenda I created and the minutes of the meeting I chaired.
+        Also ,to learn if I am effective in meetings I asked my colleagues to fill out evaluation forms after the meeting.
+    </p>
 </div>
 <div class="widget">
     <h2>Meeting agenda drawn up by me</h2>
@@ -14,10 +19,22 @@
 </div>
 <div class="widget">
     <h2>Evaluation forms from my meeting</h2>
+</div>
+<div class="widget">
     <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFormNick.pdf" type="application/pdf">
+</div>
+<div class="widget">
     <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromAlex.pdf" type="application/pdf">
+</div>
+<div class="widget">
     <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromAmir.pdf" type="application/pdf">
+</div>
+<div class="widget">
     <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromJoey.pdf" type="application/pdf">
+</div>
+<div class="widget">
     <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromOliver.pdf" type="application/pdf">
+</div>
+<div class="widget">
     <embed src="src/profSkillsPortfolio2/MeetingTechniques/MeetingEvalFromPharell.pdf" type="application/pdf">    
 </div>
