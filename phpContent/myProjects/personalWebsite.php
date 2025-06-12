063ff40a8ecc8599ca1a852067b6eb54d601377b
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-12 15:09:30 +0200

    created personal website showcase tab

--- phpContent/myProjects/personalWebsite.php
+++ phpContent/myProjects/personalWebsite.php
@@ -1,4 +1,11 @@
 <div class="widget">
-  <h1>Personal Website</h1>
-  <p>This is placeholder text for the Personal Website project.</p>
+    <h1>Personal Website</h1>
+    <p>This project is the very website you are currently viewing. It showcases my skills and projects. You can find the source code and more details on GitHub.</p>
+    <p>Built using only: HTML, CSS, JavaScript, PHP</p>
+</div>
+<div class="widget">
+    <a href="https://github.com/pop9459/personalWebsite" class="mediaLink">
+        <img src="src/img/icons/Tilda_Icons_26sn_github.svg" alt="github icon" class="icon">
+        <p>View on GitHub</p>
+    </a>
 </div>
