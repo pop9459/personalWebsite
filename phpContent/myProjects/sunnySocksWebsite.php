ebe2620ac586e7864a53f37ee7f698727a3c515f
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-12 16:06:25 +0200

    populated sunny socks tab

--- phpContent/myProjects/sunnySocksWebsite.php
+++ phpContent/myProjects/sunnySocksWebsite.php
@@ -2,5 +2,27 @@
   <h1>Sunny Socks Website</h1>
 </div>
 <div class="widget">
-  <p>This is placeholder text for the Sunny Socks Website project.</p>
+  <p>Sunny Socks is a web application, developed as a school project, designed to improve the online presence of its client by delivering an intuitive and user-friendly experience. The website aims to address both the functional needs of the client and the preferences of its users, following the principles of user-centered design.</p>
+  <p>This project encompasses the full web development cycle:
+    <ol>
+      <li>Requirements gathering and analysis</li>
+      <li>Wireframing and prototyping</li>
+      <li>Frontend development (HTML, CSS, JavaScript)</li>
+      <li>Backend development (PHP)</li>
+      <li>Usability testing and feedback implementation</li>
+    </ol>
+  </p>
+  <p>Technologies used: HTML5, CSS3, JavaScript, PHP, GitHub, Figma.</p>
+</div>
+<div class="widget">
+    <img src="src/img/sunnySocks/sunnySocks-landing-page.png" alt="Sunny Socks landing page screenshot" class="project-image">
+</div>
+<div class="widget">
+    <img src="src/img/sunnySocks/sunnySocks-shop-chatbot.png" alt="Sunny Socks shop page with chatbot screenshot" class="project-image">
+</div>
+<div class="widget">
+    <a href="https://github.com/KyanJeuring/IT1G-Website" class="mediaLink" target="_blank">
+        <img src="src/img/icons/Tilda_Icons_26sn_github.svg" alt="github icon" class="icon">
+        <p>View on GitHub</p>
+    </a>
 </div>
