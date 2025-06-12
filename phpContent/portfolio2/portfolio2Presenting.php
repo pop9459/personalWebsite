bd0bf4c3566c0fac5bf5b62b321168c75cd25fb5
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-12 20:32:21 +0200

    added presentation stuff

--- phpContent/portfolio2/portfolio2Presenting.php
+++ phpContent/portfolio2/portfolio2Presenting.php
@@ -2,12 +2,19 @@
     <h1>Portfolio 2 Presenting</h1>
 </div>
 <div class="widget">
-    <p>Placeholder for a short description of the presenting content.</p>
-    
+    <p>
+        this is a powerpoint presentation used to present this website and my second semester portfolio 
+    </p>
+</div>
+<div class="widget">
+    <h2>Presentation powerpoint</h2>
+    <embed src="src/profSkillsPortfolio2/Presenting/ProfessionalSkillsPortfolioPresentation.pdf" type="application/pdf" >
+</div>
+<div class="widget">
+    <h2>Feedback form from previous presentation</h2>
     <div class="widget">
         <h2>404 Not Found</h2>
         <!-- Yes, I lost the paper -->
         <p>The content you are looking for is not available.</p>
     </div>
-
 </div>
