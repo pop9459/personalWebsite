df3183674f938fe607ada64edfb45656c367131b
Peter Kapsiar <kapsiar888n@gmail.com>
2025-06-12 15:42:43 +0200

    populated maturita project tab

--- phpContent/myProjects/maturityProject.php
+++ phpContent/myProjects/maturityProject.php
@@ -2,5 +2,20 @@
   <h1>Maturity Project</h1>
 </div>
 <div class="widget">
-  <p>This is placeholder text for the Maturity Project.</p>
+  <p>This project is a 3D Tower Defense game developed in Unity as a "maturita" exam project. The game was created with a focus on functionality, playability, visual appeal, and future extensibility. Key technologies and tools used include Unity ProBuilder for 3D modeling and level design, and C# programming with Microsoft Visual Studio 2022 for game logic and scripting. The game involves strategic placement of towers to defend against waves of enemies.</p>
+</div>
+<div class="widget">
+    <img src="src/img/maturityProject/maturity-project-main-view.png" alt="Main view of the Maturity Project game" class="project-image">
+</div>
+<div class="widget">
+    <img src="src/img/maturityProject/maturity-project-play-area.png" alt="Play area of the Maturity Project game" class="project-image">
+</div>
+<div class="widget">
+    <img src="src/img/maturityProject/maturity-project-highlight-feature.png" alt="Highlight feature of the Maturity Project game" class="project-image">
+</div>
+<div class="widget">
+    <a href="https://github.com/pop9459/maturitaProject" class="mediaLink" target="_blank">
+        <img src="src/img/icons/Tilda_Icons_26sn_github.svg" alt="github icon" class="icon">
+        <p>View on GitHub</p>
+    </a>
 </div>
