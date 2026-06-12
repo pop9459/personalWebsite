14a1ad1ea23353498ac9458fb413ac3a1b09fe38
Peter Kapsiar <kapsiar888n@gmail.com>
2026-06-13 00:42:19 +0200

    added timeline tips

--- phpContent/aboutMe.php
+++ phpContent/aboutMe.php
@@ -1,6 +1,7 @@
 <?php
 function experienceTimeline()
 {
+    // reusable timeline segment
     $TIMELINE_SEGMENT = "<div class='timelineSegment'>
                         <div class='timelineTopLine'></div>
                         <div class='timelineCircle'></div>
@@ -8,10 +9,26 @@ function experienceTimeline()
                         </div>";
     $timeline = json_decode(file_get_contents("../json/experienceTimeline.json"), true);
 
-    $html = "<ol id='experienceTimeline'>";
+    // html string to return
+    $html = "";
+    
+    // timeline beginning tip text
+    $html .= "<h3 id='timelineTipText'>To infinity and beyond!</h3>";
 
+    // grid container for timeline
+    $html .= "<ol id='experienceTimeline'>";
+
+    // timeline beginning tip
+    $html .= "<div class='timelineSpacer'></div>";
+    $html .= "<div class='timelineSegment'>";
+    $html .= "<div class='timelineCircle'></div>";
+    $html .= "<div class='timelineBottomLine'></div>";
+    $html .= "</div>";
+    $html .= "<div class='timelineSpacer'></div>";
+
+    // loop through timeline items and add them to the html string
     $itterCount = 0;
-    foreach ($timeline["timeline"] as $experience) {
+    foreach (array_reverse($timeline["timeline"]) as $experience) {
         if ($itterCount % 2 == 0) {
             $html .= "<div class='timelineSpacer'></div>";
 
@@ -21,7 +38,7 @@ function experienceTimeline()
         $html .= "<li class='timelineItem'>";
         $html .= "<h3 class='timelineTitle'>" . $experience['title'] . "</h3>";
         $html .= "<h4 class='timelineTimestamp'>" . $experience['time'] . "</h4>";
-        $html .= "<p class='timelineDescription'>" . $experience['description'] . "<p>";
+        $html .= "<p class='timelineDescription'>" . $experience['description'] . "</p>";
         $html .= "</li>";
 
         if ($itterCount % 2 != 0) {
@@ -34,7 +51,21 @@ function experienceTimeline()
         $itterCount++;
     }
 
+    // Timeline beginning tip
+    $html .= "<div class='timelineSpacer'></div>";
+
+    $html .= "<div class='timelineSegment'>";
+    $html .= "<div class='timelineTopLine'></div>";
+    $html .= "<div class='timelineCircle'></div>";
+    $html .= "</div>";
+
+    $html .= "<div class='timelineSpacer'></div>";
+
+    // end of timeline container
     $html .= "</ol>";
+
+    // timeline beginning tip text
+    $html .= "<h3 id='timelineTipText'>A long time ago in a galaxy far, far away...</h3>";
     return $html;
 }
 ?>
